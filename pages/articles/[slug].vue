04be1447557c4aa5804897742e2a4fbc7df4930f
ScratchX98 <scratchx@gmx.com>
2023-08-10 22:49:33 +0200

    start work on article page

---
+++ pages/articles/[slug].vue
@@ -0,0 +1,28 @@
+<script setup>
+import { useArticleStore } from '/stores/article.js'
+
+const route = useRoute()
+
+const s = useArticleStore()
+
+onServerPrefetch(async () => {
+  await s.fill(route.params.slug)
+})
+</script>
+
+<template>
+<h1>{{s.article.title}}</h1>
+
+<p>{{s.article.content}}</p>
+
+<h3>{{ s.article.date }}</h3>
+</template>
+
+<style scoped>
+
+h1 {
+  font-size: 3rem;
+  font-weight: 900;
+}
+
+</style> 
